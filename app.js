b8f972bd6e89312d6ac48deb55c42648c7d127d2
jeatorresr <jandreatorresro@outlook.com>
2025-05-23 18:13:29 -0500

    🚀 Dockerfile added
    .

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-  res.send('¡Hola Mundo desde Node.js en Docker!');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, () => {
