cbb446750ca18422171d5cff8e1c6735b45eeb01
jeatorresr <jandreatorresro@outlook.com>
2025-05-28 18:25:38 -0500

    🎨 Create image
    .

--- app.js
+++ app.js
@@ -10,5 +10,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
