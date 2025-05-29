9dbd81f5e6d0a50612f356458c5bcb183d05c28f
jeatorresr <jandreatorresro@outlook.com>
2025-05-28 19:18:43 -0500

    ⚡️ Port modified
    .

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.status(200).send('OK')
 });
 
 app.listen(PORT, () => {
